12bf2295bfeb2505babb2d16a33ae774f1c8263c
Ankur Srivastava <ankursrivastava2070@gmail.com>
2023-12-14 12:42:10 +0530

    MongoDB is connected with local server

--- main.go
+++ main.go
@@ -1,12 +1,42 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"net/http"
+	"time"
+	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
+var client *mongo.Client
+func init() {
+	// Set up client options
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+
+	// Create a MongoDB client
+	client, err := mongo.NewClient(clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Connect to MongoDB
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to MongoDB!")
+}
 
 func main() {
+	r := mux.NewRouter()
 	// Define a handler function
+	
 	handler := func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, World!")
 	}
@@ -17,7 +47,9 @@ func main() {
 	// Start the server on port 8080
 	port := 8080
 	fmt.Printf("Server is listening on port %d...\n", port)
-	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+	err := http.ListenAndServe(fmt.Sprintf(":%d", port), r)
+
+	
 	if err != nil {
 		fmt.Println("Error:", err)
 	}
