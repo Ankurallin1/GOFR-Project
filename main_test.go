39daaab80c187dca035792ddb46b396ea241c40c
Ankur Srivastava <ankursrivastava2070@gmail.com>
2023-12-15 12:00:01 +0530

    The update and delete testing has been done and a cors is used in order to perform the put and delete operation

--- main_test.go
+++ main_test.go
@@ -12,12 +12,17 @@ import (
 
 func TestCRUDOperations(t *testing.T) {
 	app := fiber.New()
+	// Adding a cors for put and delete request	
+	app.Use(func(c *fiber.Ctx) error {
+		c.Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+		return c.Next()
+	})
 
-	// Set up your CRUD endpoints for testing
 	app.Get("/students", getStudents)
 	app.Post("/students", createStudent)
 	app.Get("/students/:id", getStudent)
-	
+	app.Put("/students/:id", updateStudent)
+	app.Delete("/students/:id", deleteStudent)
 	t.Run("CreateStudent", func(t *testing.T) {
 		// Define a sample student for testing
 		student := Student{
@@ -57,4 +62,39 @@ func TestCRUDOperations(t *testing.T) {
 	})
 
 	
+	t.Run("UpdateStudent", func(t *testing.T) {
+		// Define a sample student for testing
+		updatedStudent := Student{
+			Name:         "Ronak",
+			Email:        "Ronak.email@example.com",
+			CollegeName:  "JUET College",
+			EnrollmentNo: "201B365",
+		}
+
+		// Convert updated student to JSON
+		updatedStudentJSON, err := json.Marshal(updatedStudent)
+		assert.NoError(t, err)
+
+		// Create a PUT request with the updated student JSON
+		req := httptest.NewRequest(http.MethodPut, "/students/657ad262660b9c2342474c33", bytes.NewReader(updatedStudentJSON))
+		req.Header.Set("Content-Type", "application/json")
+
+		// Create a response recorder to record the response
+		res, err := app.Test(req)
+		assert.NoError(t, err)
+
+		assert.Equal(t, http.StatusOK, res.StatusCode)
+	})
+
+	t.Run("DeleteStudent", func(t *testing.T) {
+		// Create a DELETE request to delete a student
+		req := httptest.NewRequest(http.MethodDelete, "/students/657be72ea13c844c358f83b2", nil)
+
+		// Create a response recorder to record the response
+		res, err := app.Test(req)
+		assert.NoError(t, err)
+
+		assert.Equal(t, http.StatusOK, res.StatusCode)
+	})
+
 }
